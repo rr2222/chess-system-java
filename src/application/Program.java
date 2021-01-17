e19cd961d4a9da054999c6f9c722f0cd9880265a
unknown <ruither.carvalho@gmail.com>
2021-01-17 14:46:53 -0300

    Primeira classe posiçao

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.print("I'am alive");
+		Position pos = new Position(3, 5);
+		
+		System.out.println(pos);
 
 	}
 
